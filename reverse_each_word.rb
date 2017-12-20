0468d7ffa41cb003375b5e03918ba427a30a2dfa
github username <github email address>
2017-12-20 16:28:51 -0500

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,8 @@
 
 def reverse_each_word(sentence)
   new_Sent = sentence.split
-  rev_Array= []
 
-  new_Sent.collect do |words|
-    rev_Array << words.reverse
-  end
-    rev_Array.join(" ")
+  reverse_Array = new_Sent.collect {|words| words.reverse}.join(" ")
+
 
 end
